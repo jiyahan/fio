c8ed2ff63d66beb7e705ad8da88be618c65cb000
Igor Khasilev <igor.khasilev@gmail.com>
2015-07-06 20:01:01 +0300

    switch to App()

--- source/app.d
+++ source/app.d
@@ -3,31 +3,35 @@ import std.datetime;
 import std.typecons;
 import std.experimental.logger;
 
-import fio: runEventLoop, stopEventLoop, fioTask, fioSleep, fioTCPListener, fioTCPConnection;
+import fio: fioTCPListener, fioTCPConnection, fioSleep, runEventLoop, makeApp, stacksize;
 
 void main()
 {
     globalLogLevel(LogLevel.info);
-    auto dumbServer = new fioTCPListener("localhost", 9999, (fioTCPConnection c) {
-            // nothing, dumb
-            c.close();
-        });
+    stacksize = 64*1024;
 
-    auto task = new fioTask({
-        info("sleep for 1 seconds");
-        fioSleep(1.seconds);
-        info("wakeup");
-        int i;
-        for(i=0;i<1_000;i++) {
-            auto c = new fioTCPConnection("127.0.0.1", cast(ushort)9999);
-            assert(c.connected);
-            fioSleep(1.msecs);
-            c.close();
-        }
-        writeln("DONE");
-        fioSleep(100.seconds);
-        stopEventLoop();
-    });
+    makeApp((string s){
+            infof("Got args %s", s);
+            auto dumbServer = new fioTCPListener("localhost", 9999, (fioTCPConnection c) {
+                    byte[64] buf;
+                    c.recv(buf);
+                    infof("got  '%s'", cast(string)buf);
+                    c.close();
+            });
+
+            for(int i=0;i<10;i++) {
+                auto c = scoped!fioTCPConnection("127.0.0.1", cast(ushort)9999);
+                assert(c.connected);
+                infof("send '%s'", s);
+                c.send(s);
+                fioSleep(1.msecs);
+                c.close();
+            }
+            info("App done");
+        },
+    "Hello"
+    );
+
     info("running loop");
     runEventLoop();
     info("done");
